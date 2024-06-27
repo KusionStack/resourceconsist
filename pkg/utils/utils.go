face57fcee3565508c8dc29aeadf701ea2987ff9
WeichengWang1 <124144418+WeichengWang1@users.noreply.github.com>
2024-06-27 11:57:29 +0800

    feat: support deploy resourceconsist indivisually (#40)
    * feat: support deploy resourceconsist indivisually as a controller&webhook
    
    * feat: add Dockerfile

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -17,11 +17,22 @@ limitations under the License.
 package utils
 
 import (
+	"crypto"
 	"crypto/md5"
+	cryptorand "crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/hex"
+	"encoding/pem"
+	"errors"
 	"fmt"
+	"math"
+	"math/big"
 	"sync"
+	"time"
 
+	certutil "k8s.io/client-go/util/cert"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"kusionstack.io/kube-api/apps/v1alpha1"
@@ -39,6 +50,10 @@ func GenerateLifecycleFinalizer(employerName string) string {
 
 const (
 	SlowStartInitialBatchSize = 1
+
+	certificateBlockType = "CERTIFICATE"
+	rsaKeySize           = 2048
+	duration365d         = time.Hour * 24 * 365
 )
 
 func intMin(l, r int) int {
@@ -90,4 +105,51 @@ func SlowStartBatch(count int, initialBatchSize int, shortCircuit bool, fn func(
 		remaining -= batchSize
 	}
 	return successes, gotErr
+}
+
+// NewPrivateKey creates an RSA private key
+func NewPrivateKey() (*rsa.PrivateKey, error) {
+	return rsa.GenerateKey(cryptorand.Reader, rsaKeySize)
+}
+
+// EncodeCertPEM returns PEM-endcoded certificate data
+func EncodeCertPEM(cert *x509.Certificate) []byte {
+	block := pem.Block{
+		Type:  certificateBlockType,
+		Bytes: cert.Raw,
+	}
+	return pem.EncodeToMemory(&block)
+}
+
+// NewSignedCert creates a signed certificate using the given CA certificate and key
+func NewSignedCert(cfg *certutil.Config, key crypto.Signer, caCert *x509.Certificate, caKey crypto.Signer) (*x509.Certificate, error) {
+	serial, err := cryptorand.Int(cryptorand.Reader, new(big.Int).SetInt64(math.MaxInt64))
+	if err != nil {
+		return nil, err
+	}
+	if len(cfg.CommonName) == 0 {
+		return nil, errors.New("must specify a CommonName")
+	}
+	if len(cfg.Usages) == 0 {
+		return nil, errors.New("must specify at least one ExtKeyUsage")
+	}
+
+	certTmpl := x509.Certificate{
+		Subject: pkix.Name{
+			CommonName:   cfg.CommonName,
+			Organization: cfg.Organization,
+		},
+		DNSNames:     cfg.AltNames.DNSNames,
+		IPAddresses:  cfg.AltNames.IPs,
+		SerialNumber: serial,
+		NotBefore:    caCert.NotBefore,
+		NotAfter:     time.Now().Add(duration365d).UTC(),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  cfg.Usages,
+	}
+	certDERBytes, err := x509.CreateCertificate(cryptorand.Reader, &certTmpl, caCert, key.Public(), caKey)
+	if err != nil {
+		return nil, err
+	}
+	return x509.ParseCertificate(certDERBytes)
 }
